9743f6edb1a075416707d5d06697c880ff6f44c7
Sunny-2610 <sunnysinha9884@gmail.com>
2025-11-30 11:57:44 +0530

    get api done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+
+Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
+   // return view('portfolio');
+     return $firstname . " " . $lastname;
+});
