6e2929fbe92072461995c273f7953db90ea39377
Sunny-2610 <sunnysinha9884@gmail.com>
2025-11-30 13:02:44 +0530

    got a bug in post request

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,23 @@
-<h1>Home Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Tutorail</title>
+</head>
+<body>
+    <h1>Home</h1>
+    <form action="/formsubmitted" method="post >
+    @crsf
+        <label for="fullname">Fullname</label>
+        <input type="text" id="fullname" name="fullname" placeholder="Type your fullname" required>
+        <br> <br>
+
+        <label for="email">Email</label>
+  <input type="text" id="email" name="email" placeholder="Type your email" required>
+
+        <button type="submit">Submit</button>
+
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname)
    // return view('portfolio');
      return $firstname . " " . $lastname;
 });
+
+
+
+//post routes
+
+Route::post("/formsubmitted", function () {
+    return "form submitted";
+});
